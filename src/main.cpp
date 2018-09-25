105dce3f034ab1888039255bb83609ceb1785407
Marco Bacis <marco.bacis@mail.polimi.it>
2018-09-25 11:42:07 +0200

    Add external loop for each ball in the time step, fixed interruptions for no ball time

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <iostream>
+#include <algorithm>
 #include <experimental/filesystem>
 #include <fstream>
 #include <vector>
@@ -19,11 +20,12 @@
 #include "gameio.h"
 #include "utils.h"
 
-#define START_FIRST 10753295594424116
-#define END_FIRST 12557295594424116
+#define START_FIRST   10753295594424116
+#define END_FIRST     12557295594424116
+#define NO_BALL_START 12398000000000000
 
-#define START_SECOND 13086639146403495
-#define END_SECOND 14879639146403495
+#define START_SECOND  13086639146403495
+#define END_SECOND    14879639146403495
 
 using namespace std;
 
@@ -57,8 +59,8 @@ int main(int argc, char **argv) {
     game_events.push_back({2010, INT_BEGIN, 0, 0});
     game_events.push_back({2011, INT_END, START_FIRST, 0});
     loadRefereeCSV(basepath / fs::path("referee-events/interruptions/1stHalf.csv"), game_events, START_FIRST);
-    //add interruption between games
-    game_events.push_back({2010, INT_BEGIN, END_FIRST, 35});
+    //add interruption between games + no ball time
+    game_events.push_back({2010, INT_BEGIN, NO_BALL_START, 35});
     game_events.push_back({2011, INT_END, START_SECOND, 35});
     loadRefereeCSV(basepath / fs::path("referee-events/interruptions/2ndHalf.csv"), game_events, START_SECOND);
     //add end game interruption
@@ -91,6 +93,9 @@ int main(int argc, char **argv) {
     //HERE GOES ALGORITHM
     //bruteforce algorithm
 
+    int tot_ball = 0;
+    int tot_rec = 0;
+
     int real = 0;
     for(vector<sensor_record> step : game_records) {
 
@@ -102,32 +107,39 @@ int main(int argc, char **argv) {
 
 
         if(!interrupted) {
-            //get ball position (the first it finds....wrong but a first attempt)
-            sensor_record ball;
+            //get balls positions
+            vector<sensor_record> step_balls;
             for(sensor_record rec : step) {
                 if(balls->find(rec.sid) != balls->end()) {
-                    ball = rec;
-                    break;
+                    step_balls.push_back(rec);
                 }
             }
 
-            //get nearest sensor
-            float mindist = std::numeric_limits<float>::infinity();
-            int nearid = 0;
-            for(sensor_record rec : step) {
-                if (rec.sid != ball.sid && sensorPlayerIdx[rec.sid] != referee_idx) {
-                    float dist = distance(ball, rec);
-                    if(dist < mindist) {
-                        nearid = rec.sid;
-                        mindist = dist;
+            tot_rec += step.size() - step_balls.size();
+            tot_ball += step_balls.size();
+
+            float toAdd = 0.005 / step_balls.size();
+
+            for(sensor_record ball : step_balls) {
+                //get nearest sensor
+                float mindist = std::numeric_limits<float>::infinity();
+                int nearid = 0;
+                for (sensor_record rec : step) {
+                    if (balls->find(rec.sid) == balls->end() &&
+                        sensorPlayerIdx[rec.sid] != referee_idx) {
+                        float dist = distance(ball, rec);
+                        if (dist < mindist) {
+                            nearid = rec.sid;
+                            mindist = dist;
+                        }
                     }
                 }
-            }
 
-            if(mindist != std::numeric_limits<float>::infinity() && mindist < K*1000) {
-                real++;
-                possession[sensorPlayerIdx[nearid]] += 0.005;
-                //DBOUT << "Player: " << players[sensorPlayerIdx[nearid]].name << " distance: " << mindist << "\n";
+                if (mindist != std::numeric_limits<float>::infinity() && mindist < K * 1000) {
+                    real++;
+                    possession[sensorPlayerIdx[nearid]] += toAdd;
+                    //DBOUT << "Player: " << players[sensorPlayerIdx[nearid]].name << " distance: " << mindist << "\n";
+                }
             }
         }
     }
@@ -150,8 +162,7 @@ int main(int argc, char **argv) {
 
             float player_possession = 0;
             for (int pe = 0; pe < poss_events.size(); pe += 2) {
-                float tempposs = poss_events[pe + 1].ts - poss_events[pe].ts;
-                player_possession += tempposs > 0 ? tempposs : 0;
+                player_possession += poss_events[pe + 1].ts - poss_events[pe].ts;
             }
             player_possession /= 1000000000000;
             total_possession += player_possession;
@@ -170,7 +181,7 @@ int main(int argc, char **argv) {
 
     float interr = 0;
     for(int e = 2; e < game_events.size()-2;e += 2) {
-        interr += (game_events[e+1].ts - game_events[e].ts);
+        interr += game_events[e+1].ts - game_events[e].ts;
     }
     interr /= 1000000000000;
 
@@ -180,4 +191,6 @@ int main(int argc, char **argv) {
     DBOUT << "Possession real: " << total_possession << " actual " << total_actual << "\n";
     DBOUT << "Interruptions " << interr << "\n";
     DBOUT << "Total time (included interruptions) " << tot_time << " seconds\n";
+
+    DBOUT << "Total player records " << tot_rec << ", total ball records " << tot_ball << "\n";
 }
